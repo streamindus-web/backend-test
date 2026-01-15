0662ed4426a5fe5428d4eff3fcd119bff374028f
Akshar Saxena <aksharsaxena2003@gmail.com>
2026-01-15 22:19:47 +0530

    changed config

--- controllers/brochure.controller.ts
+++ controllers/brochure.controller.ts
@@ -43,7 +43,8 @@ function brochureController(req: RequestBody, res: any) {
 
         const transporter = nodemailer.createTransport({
             host: "smtp.gmail.com",
-         port: 587,
+            port: 587,
+            secure: false,
             auth: {
                 user: process.env.EMAIL_USER,
                 pass: process.env.EMAIL_PASS,
